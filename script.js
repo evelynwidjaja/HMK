a884de80efa734094055e302c4b79433438543a5
Evelyn <evelynwidjaja22@gmail.com>
2025-05-23 22:29:09 +0700

    responsive ver

--- script.js
+++ script.js
@@ -11,4 +11,13 @@ function togglePlay() {
     }
 }
 
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('navLinks');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+
+
+
 
