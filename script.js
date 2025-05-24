3b0c93f49060143e1a0c7c09a6fc618761e1f3db
Evelyn <evelynwidjaja22@gmail.com>
2025-05-24 11:40:36 +0700

    change the video button

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ function togglePlay() {
 
     if (video.paused) {
         video.play();
-        button.innerHTML = "⏸";
     } else {
         video.pause();
-        button.innerHTML = "▶";
     }
 }
 
